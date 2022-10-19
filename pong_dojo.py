ceb8fb6cebe7bdc778fb005f7b0188dca2d66e22
Marek Matas <matas.marek1@gmail.com>
2022-10-19 23:05:07 +0200

    palky se kresli, zatim nehejbou

--- pong_dojo.py
+++ pong_dojo.py
@@ -1,1 +1,55 @@
-import numpy as np
+import pygame
+pygame.init()
+
+WIDTH, HEIGHT = 700, 500
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+
+PADDLE_WIDTH, PADDLE_HEIGHT = 20, 100
+
+FPS = 60
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+
+class Paddle:
+    COLOR = WHITE
+    VEL = 4
+
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+
+    def draw(self, win):
+        pygame.draw.rect(win, self.COLOR, (self.x, self.y, self.width, self.height))
+
+
+def draw(win, paddles):
+    win.fill(BLACK)
+    for paddle in paddles:
+        paddle.draw(win)
+    pygame.display.update()
+
+
+def main():
+    run = True
+    clock = pygame.time.Clock()
+    left_paddle = Paddle(10, HEIGHT//2 - PADDLE_HEIGHT//2, PADDLE_WIDTH, PADDLE_HEIGHT)
+    right_paddle = Paddle(WIDTH - 10 - PADDLE_WIDTH, HEIGHT//2 - PADDLE_HEIGHT//2, PADDLE_WIDTH, PADDLE_HEIGHT)
+
+    while run:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+
+        draw(WIN, [left_paddle, right_paddle])
+    return
+
+
+if __name__ == '__main__':
+    main()
+
