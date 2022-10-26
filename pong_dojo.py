82d078c7646128acd9fe32f7c1fb659a7c1fd775
Marek Matas <matas.marek1@gmail.com>
2022-10-26 23:09:33 +0200

    Ball included, collisions done

--- pong_dojo.py
+++ pong_dojo.py
@@ -5,7 +5,7 @@ WIDTH, HEIGHT = 700, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 
 PADDLE_WIDTH, PADDLE_HEIGHT = 20, 100
-
+BALL_RADIUS = 7
 FPS = 60
 
 WHITE = (255, 255, 255)
@@ -13,31 +13,96 @@ BLACK = (0, 0, 0)
 
 
 class Paddle:
-    COLOR = WHITE
     VEL = 4
 
-    def __init__(self, x, y, width, height):
+    def __init__(self, x, y, width, height, color):
+        self.color = color
         self.x = x
         self.y = y
         self.width = width
         self.height = height
 
     def draw(self, win):
-        pygame.draw.rect(win, self.COLOR, (self.x, self.y, self.width, self.height))
+        pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.height))
+
+    def move(self, up=True):
+        if up:
+            self.y -= self.VEL
+        else:
+            self.y += self.VEL
 
 
-def draw(win, paddles):
+class Ball:
+    MAX_VEL = 5
+    COLOR = WHITE
+
+    def __init__(self, x, y, radius):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.x_vel = self.MAX_VEL
+        self.y_vel = 0
+
+    def move(self):
+        self.x += self.x_vel
+        self.y += self.y_vel
+
+    def draw(self, win):
+        pygame.draw.circle(win, self.COLOR, (self.x, self.y), self.radius)
+
+
+def draw(win, paddles, ball):
     win.fill(BLACK)
     for paddle in paddles:
         paddle.draw(win)
+    ball.draw(win)
     pygame.display.update()
 
 
+def handle_paddle_movement(keys, left_paddle, right_paddle):
+    if keys[pygame.K_w] and left_paddle.y - left_paddle.VEL >= 0:
+        left_paddle.move(up=True)
+    if keys[pygame.K_s] and left_paddle.y + left_paddle.VEL + left_paddle.height <= HEIGHT:
+        left_paddle.move(up=False)
+
+    if keys[pygame.K_UP] and right_paddle.y - right_paddle.VEL >= 0:
+        right_paddle.move(up=True)
+    if keys[pygame.K_DOWN] and right_paddle.y + right_paddle.VEL + right_paddle.height <= HEIGHT:
+        right_paddle.move(up=False)
+
+
+def handle_collision(ball, left_paddle, right_paddle):
+    if ball.y - ball.radius <= 0:
+        ball.y_vel *= -1
+    elif ball.y + ball.radius >= HEIGHT:
+        ball.y_vel *= -1
+
+    if ball.x_vel < 0:
+        if ball.y >= left_paddle.y and ball.y <= left_paddle.y + left_paddle.height:
+            if ball.x - ball.radius <= left_paddle.x + left_paddle.width:
+                ball.x_vel *= -1
+
+                middle_y = left_paddle.y + left_paddle.height/2
+                difference_in_y = ball.y - middle_y
+                relative_distance = difference_in_y / (left_paddle.height/2)
+                ball.y_vel = relative_distance * ball.MAX_VEL
+
+    else:
+        if ball.y >= right_paddle.y and ball.y <= right_paddle.y + right_paddle.height:
+            if ball.x + ball.radius >= right_paddle.x:
+                ball.x_vel *= -1
+
+                middle_y = right_paddle.y + right_paddle.height/2
+                difference_in_y = ball.y - middle_y
+                relative_distance = difference_in_y / (right_paddle.height/2)
+                ball.y_vel = relative_distance * ball.MAX_VEL
 def main():
     run = True
     clock = pygame.time.Clock()
-    left_paddle = Paddle(10, HEIGHT//2 - PADDLE_HEIGHT//2, PADDLE_WIDTH, PADDLE_HEIGHT)
-    right_paddle = Paddle(WIDTH - 10 - PADDLE_WIDTH, HEIGHT//2 - PADDLE_HEIGHT//2, PADDLE_WIDTH, PADDLE_HEIGHT)
+    left_paddle = Paddle(10, HEIGHT//2 - PADDLE_HEIGHT//2, PADDLE_WIDTH, PADDLE_HEIGHT, WHITE)
+    right_paddle = Paddle(WIDTH - 10 - PADDLE_WIDTH, HEIGHT//2 - PADDLE_HEIGHT//2, PADDLE_WIDTH, PADDLE_HEIGHT, WHITE)
+
+    ball = Ball(WIDTH//2, HEIGHT//2, BALL_RADIUS)
 
     while run:
         clock.tick(FPS)
@@ -46,10 +111,20 @@ def main():
                 run = False
                 break
 
-        draw(WIN, [left_paddle, right_paddle])
+        ball.move()
+        handle_collision(ball, left_paddle, right_paddle)
+
+        keys = pygame.key.get_pressed()
+        handle_paddle_movement(keys, left_paddle, right_paddle)
+
+        draw(WIN, [left_paddle, right_paddle], ball)
     return
 
 
 if __name__ == '__main__':
     main()
+
+# TODO: Bug, ball can enter the paddle. We first move the ball, then invert speed, then draw.
+# TODO: Bug, if paddle is 3px below the screen edge, it does not move all the way to the edge.
+# TODO: Game reset: Ball in the middle, moving towards the losing player, reset paddle location.
 
